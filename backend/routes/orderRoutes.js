d9ec71f14c6d5694536f2523b4b6e6133fc0ea6c
fantaser <fantaser_tariq@yahoo.com>
2021-05-28 14:56:04 +0500

    pay

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,21 +1,19 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 //const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../config/keys");
-const passport = require("passport");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+const passport = require('passport');
 // const multer = require('multer')
 // const fs = require("fs");
 // var path = require('path')
-
 
 //Load Input Validation
 //const validateRegisterInput = require("../../validation/register");
 //const validateLoginInput = require("../../validation/login");
 //Load User Model
-const Order = require("../models/orderModel");
-
+const Order = require('../models/orderModel');
 
 // var storage = multer.diskStorage({
 //     destination: function (req, file, cb) {
@@ -25,96 +23,77 @@ const Order = require("../models/orderModel");
 //       cb(null, Date.now() + path.extname(file.originalname))
 //     }
 //   })
-   
-//   var upload = multer({ storage: storage })
 
+//   var upload = multer({ storage: storage })
 
 //   router.post('/imageupload',auth2, upload.single('resourceMaterial'), async (req,res) => {
 //     let userId = req.body.userID
 //     let productId = req.body.productId
 //     let resourceMaterial = req.file.filename
 
-    
 //     try {
 //         // if (!file) {
 //         //     const error = new Error('Please upload a file')
 //         //     error.httpStatusCode = 400
-//         //     console.log(error) 
+//         //     console.log(error)
 //         //   }
-        
+
 //         res.status(200).send('Inserted assignment')
 //     } catch (error) {
 //         res.status(400).send(error)
 //     }
 // })
 
-
-router.get("/test", (req, res) =>
-    res.json({
-        msg: "User Works"
-    })
+router.get('/test', (req, res) =>
+  res.json({
+    msg: 'User Works',
+  })
 );
 
+router.post('/addorder', (req, res) => {
+  console.log('sdsa', req.body);
+  console.log('done');
 
-router.post("/addorder", (req, res) => {
-    console.log("sdsa",req.body)
-    console.log("done")
-   
+  console.log(req.body, 'fasda');
+  const newOrder = new Order({
+    user: req.body.user,
+    product: req.body.product,
+    address: req.body.address,
+    city: req.body.city,
+    zipcode: req.body.zipcode,
+  });
+  //console.log(newUser.perm);
 
-            console.log(req.body,"fasda");
-            const newOrder = new Order({
-
-
-                user:req.body.user,
-                product:req.body.product,
-                address:req.body.address,
-                city:req.body.city,
-                zipcode:req.body.zipcode,
-              
-                
-
-            });
-            //console.log(newUser.perm);
-        
-                    newOrder
-                        .save()
-                        .then(order => res.json({order,error:"Order Placed Successfully"}))
-                        .catch(err => console.log(err));
-                
-     
+  newOrder
+    .save()
+    .then((order) => res.json({ order, error: 'Order Placed Successfully' }))
+    .catch((err) => console.log(err));
 });
 
+router.post('/addpayment/:id', (req, res) => {
+  try {
+    const id = req.params.id;
+    const { imageUrl } = req.body;
+    const order = Order.findByIdAndUpdate(id, { $set: { imageUrl: imageUrl } });
 
-router.post("/addpayment", (req, res) => {
-    console.log("sdsa",req.body)
-    console.log("done")
-   
-            console.log(req.body,"fasda");
-            const newProduct = new Product({
-                user:req.body.user,
-                title:req.body.title,
-                image:req.body.image,
-                category:req.body.category,
-                subCategory:req.body.subCategory,
-                description:req.body.description,
-                price:req.body.price,
-                productLocation:req.body.productLocation,
-                itemCondition:req.body.itemCondition
-                
+    return res.json({ order, msg: 'Payment Sent' });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-            });
-            //console.log(newUser.perm);
-        
-                    newProduct
-                        .save()
-                        .then(product => res.json({product,error:"Product Successfully added"}))
-                        .catch(err => console.log(err));
-                
-     
+router.post('/getAllOrders', (req, res) => {
+  try {
+    const order = Order.find();
+
+    return res.json(order);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // router.get("/getAllProducts",
-//     /*   
+//     /*
 //     passport.authenticate("jwt", {
 //             session: false
 //         }),
@@ -149,8 +128,5 @@ router.post("/addpayment", (req, res) => {
 //                 })
 //             );
 //     });
-
-
-
 
 module.exports = router;
--- src/config/router.js
+++ src/config/router.js
@@ -1,70 +1,70 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Switch, withRouter } from "react-router-dom";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter,
+} from 'react-router-dom';
 
-import Header from '../Components/Header.jsx'
-import About from '../Components/About.jsx'
+import Header from '../Components/Header.jsx';
+import About from '../Components/About.jsx';
 // import Login from '../Components/LoginModal'
-import Chat from '../Components/Chat'
-import Order from '../Components/Order'
-import Home from '../Components/Home'
-import Sell from '../Components/Sell'
-import Attributes from '../Components/Attributes'
-import PhoneLogin from '../Components/PhoneAuth'
-import Success from '../Components/Success'
-import AdDetails from '../Components/AdDetails'
-import MyAds from '../Components/MyAds'
-import Types from '../Components/Types'
-import Footer from '../Components/Footer'
-import Signup from '../Components/Signup'
-import Login from '../Components/Login'
-import RiderSignup from '../Components/RiderSignUp'
-import NewHeader from '../Components/NewHeader'
-import NewSidebar from '../Components/NewSidebar'
-import Carousal from '../Components/Carousal'
-import OrderPage from "../Components/OrderPage";
-import FindRider from '../Components/FindRider'
-import SendRiderLocation from "../Components/SendRiderLocation";
-import OrderPlaced from "../Components/OrderPlaced"
-import PaymentMethod from "../Components/PaymentMethod"
-import JazzCash from "../Components/JazzCash"
-import EasyPaisa from "../Components/EasyPaisa"
-import BankTransfer from "../Components/BankTransfer"
-
+import Chat from '../Components/Chat';
+import Order from '../Components/Order';
+import Home from '../Components/Home';
+import Sell from '../Components/Sell';
+import Attributes from '../Components/Attributes';
+import PhoneLogin from '../Components/PhoneAuth';
+import Success from '../Components/Success';
+import AdDetails from '../Components/AdDetails';
+import MyAds from '../Components/MyAds';
+import Types from '../Components/Types';
+import Footer from '../Components/Footer';
+import Signup from '../Components/Signup';
+import Login from '../Components/Login';
+import RiderSignup from '../Components/RiderSignUp';
+import NewHeader from '../Components/NewHeader';
+import NewSidebar from '../Components/NewSidebar';
+import Carousal from '../Components/Carousal';
+import OrderPage from '../Components/OrderPage';
+import FindRider from '../Components/FindRider';
+import SendRiderLocation from '../Components/SendRiderLocation';
+import OrderPlaced from '../Components/OrderPlaced';
+import PaymentMethod from '../Components/PaymentMethod';
+import JazzCash from '../Components/JazzCash';
+import EasyPaisa from '../Components/EasyPaisa';
+import BankTransfer from '../Components/BankTransfer';
 
 class AppRouter extends React.Component {
-    render() {
-        return (
-
-
-                  <Router>
-                      <Route exact path="/" component={Home} />
-                      <Route exact path="/Login" component={Login} />
-                      <Route exact path="/chat" component={Chat} />
-                      <Route exact path="/order" component={Order} />
-                      <Route exact path="/sell" component={Sell} />
-                      <Route exact path="/sell/attribute" component={Attributes} />
-                      <Route exact path="/phoneauth" component={PhoneLogin} />
-                      <Route exact path="/success" component={Success} />
-                      <Route exact path="/AdDetails" component={AdDetails} />
-                      <Route exact path="/myads" component={MyAds} />
-                      <Route exact path="/types" component={Types} />
-                      <Route exact path="/footer" component={Footer} />
-                      <Route exact path="/Signup" component={Signup} />
-                      <Route exact path="/RiderSignup" component={RiderSignup} />
-                      <Route exact path="/NewHeader" component={NewHeader} />
-                      <Route exact path="/Carousal" component={Carousal} />
-                      <Route exact path="/OrderPage" component={OrderPage}/>
-                      <Route exact path="/FindRider" component={FindRider}/>
-                      <Route exact path="/SendRiderLocation" component={SendRiderLocation}/>
-                      <Route exact path="/OrderPlaced" component={OrderPlaced}/>
-                      <Route exact path="/PaymentMethod" component={PaymentMethod}/>
-                      <Route exact path="/JazzCash" component={JazzCash}/>
-                      <Route exact path="/EasyPaisa" component={EasyPaisa}/>
-                      <Route exact path="/BankTransfer" component={BankTransfer}/>
-                     
-                  </Router>
-
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/Login' component={Login} />
+        <Route exact path='/chat' component={Chat} />
+        <Route exact path='/order' component={Order} />
+        <Route exact path='/sell' component={Sell} />
+        <Route exact path='/sell/attribute' component={Attributes} />
+        <Route exact path='/phoneauth' component={PhoneLogin} />
+        <Route exact path='/success' component={Success} />
+        <Route exact path='/AdDetails' component={AdDetails} />
+        <Route exact path='/myads' component={MyAds} />
+        <Route exact path='/types' component={Types} />
+        <Route exact path='/footer' component={Footer} />
+        <Route exact path='/Signup' component={Signup} />
+        <Route exact path='/RiderSignup' component={RiderSignup} />
+        <Route exact path='/NewHeader' component={NewHeader} />
+        <Route exact path='/Carousal' component={Carousal} />
+        <Route exact path='/OrderPage' component={OrderPage} />
+        <Route exact path='/FindRider' component={FindRider} />
+        <Route exact path='/SendRiderLocation' component={SendRiderLocation} />
+        <Route exact path='/OrderPlaced' component={OrderPlaced} />
+        <Route exact path='/PaymentMethod/:id' component={PaymentMethod} />
+        <Route exact path='/JazzCash' component={JazzCash} />
+        <Route exact path='/EasyPaisa' component={EasyPaisa} />
+        <Route exact path='/BankTransfer' component={BankTransfer} />
+      </Router>
+    );
+  }
 }
 export default AppRouter;
